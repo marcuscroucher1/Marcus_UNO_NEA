0e2746c6c6294b2c9b2b9cfd4fd788efd22e8e63
Marcus <marcusbcroucher@protonmail.com>
2023-01-24 16:07:40 +0000

    main8.py now can display a card with matching image based on a simple hardcoded search in array.

--- main8.py
+++ main8.py
@@ -4,26 +4,43 @@ import random
 
 class Card:
 
-    def __init__(self, colour, symbol, owner):
+    def __init__(self, colour, symbol):
         self.__colour = colour
         self.__symbol = symbol
         self.__image = pygame.image.load("assets/" + self.__colour + "_" + self.__symbol + ".png")
-        self.__owner = owner
+        self.__owner = None
 
     def get_colour(self):
         return self.__colour
 
-    def get_number(self):
-        return self.__number
+    def get_symbol(self):
+        return self.__symbol
 
     def assign_owner(self, owner):
         self.__owner = owner
 
     def display_details(self):
-        print(self.__colour, self.__number, self.__owner)
+        print(self.__colour, self.__number, self.__owner) #
 
     def __str__(self):
-        return self.__colour + str(self.__number) + str(self.__owner)
+        return self.__colour + str(self.__symbol) + str(self.__image)
+
+    def print_card(self): # this print card funtion exists to display the image of the card which is
+        pygame.init()
+        X = 388
+        Y = 562
+        screen = pygame.display.set_mode((X, Y))
+        pygame.display.set_caption('image')
+        image = self.__image
+        screen.blit(image, (0, 0))
+        pygame.display.flip()
+        status = True
+        while (status):
+            for i in pygame.event.get():
+                if i.type == pygame.QUIT:
+                    status = False
+        pygame.quit()
+
 
 
 class Deck:
@@ -36,14 +53,16 @@ class Deck:
 
         for possible_colour in filename_colours:
             for possible_symbol in possible_symbols:
-                image = "assets/" + possible_colour + "_" + possible_symbol + ".png"
-                colour = possible_colour
-                symbol = possible_symbol
-                print("")
-                print(image)
-                print(colour)
-                print(symbol)
+                card_obj = Card(colour=possible_colour, symbol=possible_symbol)
+                list_of_cards.append(card_obj)
+        for card in list_of_cards: # picks the card that matches the one according to the line underneath this
+            if card.get_symbol() == '5' and card.get_colour() == 'Yellow': # will find a card that is of number 5 and
+                # colour yellow, and if exists, prints its matching image using the print function.
+                card.print_card()
 
 
-obj = Deck()
+obj = Deck()    # creates an object(think like a table) called obj, and creates it using the 'instructions' set out by the Deck Class.
+
+
+
 
