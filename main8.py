8b6a679f7ef9d4c0dcd09ff5324e6f0287e60f34
Marcus <marcusbcroucher@protonmail.com>
2023-01-24 15:18:22 +0000

    added object for cards and created main8.py

--- main8.py
+++ main8.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 
+
 class Card:
 
     def __init__(self, colour, symbol, owner):
@@ -24,21 +25,25 @@ class Card:
     def __str__(self):
         return self.__colour + str(self.__number) + str(self.__owner)
 
+
 class Deck:
 
+    def __init__(self):
+        list_of_cards = []
 
-    list_of_cards = []
+        filename_colours = ["Blue", "Green", "Red", "Yellow"]
+        possible_symbols = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "Draw", "Reverse", "Skip"]
 
-    filename_colours = ["Blue", "Green", "Red", "Yellow"]
-    possible_symbols = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "Draw", "Reverse", "Skip"]
+        for possible_colour in filename_colours:
+            for possible_symbol in possible_symbols:
+                image = "assets/" + possible_colour + "_" + possible_symbol + ".png"
+                colour = possible_colour
+                symbol = possible_symbol
+                print("")
+                print(image)
+                print(colour)
+                print(symbol)
 
-    for possible_colour in filename_colours:
-        for possible_symbol in possible_symbols:
-            image = "assets/" + possible_colour + "_" + possible_symbol + ".png"
-            colour = possible_colour
-            symbol = possible_symbol
-            print("")
-            print(image)
-            print(colour)
-            print(symbol)
+
+obj = Deck()
 
